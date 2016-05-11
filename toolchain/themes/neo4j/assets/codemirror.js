0239bb26f6fbac4af51600ef6ccc9449015b18f9
Benoît Simard (@logisima) <contact@bsimard.com>
2016-05-11 13:21:54 +0200

    Correct codemirror integration

--- toolchain/themes/neo4j/assets/codemirror.js
+++ toolchain/themes/neo4j/assets/codemirror.js
@@ -1,13 +1,16 @@
-'use strict';
-
+var codes = [];
 var elements = document.getElementsByTagName("textarea");
 for( var i=0; i< elements.length; i++) {
     var element = elements[i];
-    console.log(element.getAttribute("class"));
     if ( element.getAttribute("class") ){
       var mode = element.getAttribute("class").split(" ")[0];
-      console.log(mode);
-      CodeMirror.fromTextArea(element, { 'readonly':true, 'mode':mode, 'theme':'neo', 'lineNumbers':true, 'lineWrapping':true, height:'auto'});
+      codes.push(CodeMirror.fromTextArea(element, { 'readonly':true, 'mode':mode, 'theme':'neo', 'lineNumbers':true, 'lineWrapping':true, height:'auto'}));
     }
 }
+    
+Reveal.addEventListener( 'slidechanged', function( event ) {
+    codes.forEach(function(ele) {
+      ele.refresh();
+    });
+} );
  
